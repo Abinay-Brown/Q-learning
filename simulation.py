4103c7d3e8cd22401fdb09ab9ab08092664e213b
Abinay-Brown <abinay.joel@gmail.com>
2023-10-04 21:19:07 -0400

    PID tunning

--- simulation.py
+++ simulation.py
@@ -25,44 +25,21 @@ l = 0.171;
 g = 9.81;
 
 
-
-# PID gains
-Kp_X = 0; Ki_X = 0; Kd_X = 0;
-Kp_Y = 0; Ki_Y = 0; Kd_Y = 0;
-Kp_z = 0; Ki_z = 0; Kd_z = 0;
-
-Kp_phi = 0; Ki_phi = 0; Kd_phi = 0;
-Kp_theta = 0; Ki_theta = 0; Kd_theta = 0;
-Kp_psi = 0; Ki_psi = 0; Kd_psi = 0;
-
-Kp_p = 0; Ki_p = 0; Kd_p = 0;
-Kp_q = 0; Ki_q = 0; Kd_q = 0;
-Kp_r = 0; Ki_r = 0; Kd_r = 0;
-
-# PID Error Variables
-X_err_int = 0; X_err_dot = 0; X_err_prev = 0;
-Y_err_int = 0; Y_err_dot = 0; Y_err_prev = 0;
-
-phi_err_int = 0; phi_err_dot = 0; phi_err_prev = 0;
-theta_err_int = 0; theta_err_dot = 0; theta_err_prev = 0;
-psi_err_int = 0; psi_err_dot = 0; psi_err_prev = 0;
-
-p_err_int = 0; p_err_dot = 0; p_err_prev = 0;
-q_err_int = 0; q_err_dot = 0; q_err_prev = 0;
-r_err_int = 0; r_err_dot = 0; r_err_prev = 0;
-
 # Setpoints
 X_des = 0;
 Y_des = 0;
-Z_des = 5;
+Z_des = 2;
 
 
 
 
 def main():
+    
     phi_des = 5 * (np.pi / 180)
     theta_des = 5 * (np.pi / 180)
-    psi_des = 0;
+    psi_des = 45 * (np.pi / 180);
+    X_err_int = 0; X_err_dot = 0; X_err_prev = 0;
+    Y_err_int = 0; Y_err_dot = 0; Y_err_prev = 0;
     z_err_int = 0; z_err_dot = 0; z_err_prev = 0;
     phi_err_int = 0; phi_err_dot = 0; phi_err_prev = 0;
     theta_err_int = 0; theta_err_dot = 0; theta_err_prev = 0;
@@ -72,17 +49,17 @@ def main():
     q_err_int = 0; q_err_dot = 0; q_err_prev = 0;
     r_err_int = 0; r_err_dot = 0; r_err_prev = 0;
 
-    Kp_X = 0; Ki_X = 0; Kd_X = 0;
-    Kp_Y = 0; Ki_Y = 0; Kd_Y = 0;
-    Kp_z = 0.15; Ki_z = 0.01; Kd_z = 1.0;
+    Kp_X = 0.0; Ki_X = 0.00; Kd_X = 0.;
+    Kp_Y = 0.0; Ki_Y = 0.00; Kd_Y = 0.;
+    Kp_z = 1; Ki_z = 0.0; Kd_z = 0.0;
 
-    Kp_phi = 10; Ki_phi = 0.000; Kd_phi = 0.1;
-    Kp_theta = 0; Ki_theta = 0; Kd_theta = 0;
-    Kp_psi = 0; Ki_psi = 0; Kd_psi = 0;
+    Kp_phi = 6.5; Ki_phi = 0.001; Kd_phi = 0.1;
+    Kp_theta = 6.5; Ki_theta = 0.001; Kd_theta = 0.1;
+    Kp_psi = 1.0; Ki_psi = 0.00; Kd_psi = 0.0;
 
-    Kp_p = 0.1; Ki_p = 0.000; Kd_p = 0.0;
-    Kp_q = 0; Ki_q = 0; Kd_q = 0;
-    Kp_r = 0; Ki_r = 0; Kd_r = 0;
+    Kp_p = 0.05; Ki_p = 0.0001; Kd_p = 0.0;
+    Kp_q = 0.05; Ki_q = 0.0001; Kd_q = 0.0;
+    Kp_r = 0.05; Ki_r = 0.0001; Kd_r = 0.0 ;
 
     u1 = mass * g;
     u2 = 0;
@@ -90,7 +67,7 @@ def main():
     u4 = 0;
     
     t0 = 0
-    t1 = 10
+    t1 = 20
     dt = 0.01
     t = np.arange(t0, t1, dt)
     
@@ -138,9 +115,9 @@ def main():
         Y_err_dot = (Y_err - Y_err_prev) / dt;
         Y_err_prev = Y_err;
         
-        
-        theta_des = (Kp_X * X_err) + (Ki_X * X_err_int) + (Kd_X * X_err_dot);
-        phi_des =   (Kp_Y * Y_err) + (Ki_Y * Y_err_int) + (Kd_Y * Y_err_dot);
+        if (np.mod(iter, 5) == 1):
+            theta_des = (Kp_X * X_err) + (Ki_X * X_err_int) + (Kd_X * X_err_dot);
+            phi_des =   (Kp_Y * Y_err) + (Ki_Y * Y_err_int) + (Kd_Y * Y_err_dot);
         '''
         
         z_err = z_des - z;
@@ -188,7 +165,7 @@ def main():
         
         u2 = (Kp_p * p_err) + (Ki_p * p_err_int) + (Kd_p * p_err_dot);
         u3 = (Kp_q * q_err) + (Ki_q * q_err_int) + (Kd_q * q_err_dot);
-        u3 = (Kp_r * r_err) + (Ki_r * r_err_int) + (Kd_r * r_err_dot);
+        u4 = (Kp_r * r_err) + (Ki_r * r_err_int) + (Kd_r * r_err_dot);
         Omega, w1, w2, w3, w4 = Inp2Omega(u1, u2, u3, u4, Ct, Ctheta, l)
         
         solver.integrate(t[iter])
@@ -199,13 +176,31 @@ def main():
         iter = iter + 1
         #print(state)
         
-    plt.subplot(2, 1, 1)
-    plt.plot(t, sol[:, 0])
-    plt.subplot(2, 1, 2)
-    plt.plot(t, sol[:, 3])
-    plt.show()
-    return
+    return t, sol
         
     
 
-main()
+t, sol = main()
+# Roll 
+plt.subplot(2, 3, 1)
+plt.plot(t, sol[:, 0])
+
+# Pitch
+plt.subplot(2, 3, 2)
+plt.plot(t, sol[:, 1])
+
+# Yaw
+plt.subplot(2, 3, 3)
+plt.plot(t, sol[:, 2])
+
+plt.subplot(2, 3, 4)
+plt.plot(t, sol[:, 9])
+
+plt.subplot(2, 3, 5)
+plt.plot(t, sol[:, 10])
+
+plt.subplot(2, 3, 6)
+plt.plot(t, sol[:, 11])
+
+plt.show()
+    
