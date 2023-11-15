38a703b365167eedc3942e453d17c9ada1ef5365
Abinay-Brown <abinay.joel@gmail.com>
2023-11-15 14:58:04 -0500

    Kronecker done

--- Q-learning.py
+++ Q-learning.py
@@ -3,7 +3,7 @@ import control as ct
 from params import *
 from dynamics import *
 from numpy import sin, cos, tan, ones, dot, hstack, vstack, zeros
-from numpy.linalg import norm
+from numpy.linalg import norm, inv
 from scipy.interpolate import CubicSpline
 
 
@@ -63,6 +63,8 @@ def babyCT_b(t, state, params):
     Wa20 = state[73: 79]
     Wa30 = state[79: 85]
     Wa40 = state[85: 91]
+    
+
     P = state[91]
     
     
@@ -73,7 +75,7 @@ def babyCT_b(t, state, params):
     W = Vels[2]
     
     # Q-learning state vector is global position and velocity
-    x = np.array([[X, Y, Z, U, V, W]]).T
+    x = np.array([X, Y, Z, U, V, W]).reshape(6, 1)
     
     # Updated Control
     ud = np.array([dot(Wa10.T, x-xf), dot(Wa20.T, x-xf), dot(Wa30.T, x-xf), dot(Wa40.T, x-xf)])
@@ -94,7 +96,54 @@ def babyCT_b(t, state, params):
     x_del[5, 0] = x6_del(t-T)
     p_del_val = p_del(t-T)
     
+    # Kronecker Products
+    diff = x-xf
+    U = np.array([diff[0], diff[1], diff[2], diff[3], diff[4], diff[5], ud[0], ud[1], ud[2],ud[3], ud[4]]).T
+    UkU = np.array([U[0]**2, U[0]*U[1], U[0]*U[2], U[0]*U[3], U[0]*U[4], U[0]*U[5], U[0]*U[6], U[0]*U[7], U[0]*U[8], U[0]*U[9],\
+        U[1]**2, U[1]*U[2], U[1]*U[3], U[1]*U[4], U[1]*U[5], U[1]*U[6], U[1]*U[7], U[1]*U[8], U[0]*U[9],\
+            U[2]**2, U[2]*U[3], U[2]*U[4], U[2]*U[5], U[2]*U[6], U[2]*U[7], U[2]*U[8], U[2]*U[9],\
+                U[3]**2, U[3]*U[4], U[3]*U[5], U[3]*U[6], U[3]*U[7], U[3]*U[8], U[4]*U[9],\
+                    U[4]**2, U[4]*U[5], U[4]*U[6], U[4]*U[7], U[4]*U[8], U[4]*U[9],\
+                        U[5]**2, U[5]*U[6], U[5]*U[7], U[5]*U[8], U[6]*U[9],\
+                            U[6]**2, U[6]*U[7], U[6]*U[8], U[6]*U[9],\
+                                U[7]**2, U[7]*U[8], U[7]*U[9],\
+                                    U[8]**2, U[8]*U[9],\
+                                        U[9]**2])	
+    
+    Udel = np.array([x_del[0:6, 0].T, ud_del[0:4, 0].T]).T
+    UkUdel = np.array([Udel[0]**2, Udel[0]*Udel[1], Udel[0]*Udel[2], Udel[0]*Udel[3], Udel[0]*Udel[4], Udel[0]*Udel[5], Udel[0]*Udel[6], Udel[0]*Udel[7], Udel[0]*Udel[8], Udel[0]*Udel[9],\
+        Udel[1]**2, Udel[1]*Udel[2], Udel[1]*Udel[3], Udel[1]*Udel[4], Udel[1]*Udel[5], Udel[1]*Udel[6], Udel[1]*Udel[7], Udel[1]*Udel[8], Udel[0]*Udel[9],\
+            Udel[2]**2, Udel[2]*Udel[3], Udel[2]*Udel[4], Udel[2]*Udel[5], Udel[2]*Udel[6], Udel[2]*Udel[7], Udel[2]*Udel[8], Udel[2]*Udel[9],\
+                Udel[3]**2, Udel[3]*Udel[4], Udel[3]*Udel[5], Udel[3]*Udel[6], Udel[3]*Udel[7], Udel[3]*Udel[8], Udel[4]*Udel[9],\
+                    Udel[4]**2, Udel[4]*Udel[5], Udel[4]*Udel[6], Udel[4]*Udel[7], Udel[4]*Udel[8], Udel[4]*Udel[9],\
+                        Udel[5]**2, Udel[5]*Udel[6], Udel[5]*Udel[7], Udel[5]*Udel[8], Udel[6]*Udel[9],\
+                            Udel[6]**2, Udel[6]*Udel[7], Udel[6]*Udel[8], Udel[6]*Udel[9],\
+                                Udel[7]**2, Udel[7]*Udel[8], Udel[7]*Udel[9],\
+                                    Udel[8]**2, Udel[8]*Udel[9],\
+                                        Udel[9]**2])	
+    
+    Quu = np.array([[Wc[-10], Wc[-9], Wc[-8], Wc[-7]],
+                    [Wc[-9], Wc[-6], Wc[-5], Wc[-4]],
+                    [Wc[-8], Wc[-5], Wc[-3], Wc[-2]],
+                    [Wc[-7], Wc[-4], Wc[-2], Wc[-1]]])
+    Quu_inv = inv(Quu)
+    Qux = np.array([[Wc[6], Wc[15], Wc[23], Wc[30], Wc[36], Wc[41]],
+                    [Wc[7], Wc[16], Wc[24], Wc[31], Wc[37], Wc[42]],
+                    [Wc[8], Wc[17], Wc[25], Wc[32], Wc[38], Wc[43]],
+                    [Wc[9], Wc[18], Wc[26], Wc[33], Wc[39], Wc[44]]])
 
+    # Integral Reinforcement Dynamics
+    dP = 0.5*((dot(x.T, dot(M, x)))+ dot(ud.T, dot(R, ud)))
+    sig = UkU - UkUdel
+    sig_f = UkU
+    ec = P - p_del_val + dot(Wc.T, sig)
+    ecfinal = 0.5*dot(x.T, dot(Pt, x)) - dot(Wc.T, sig_f)
+    
+    term = dot(Quu_inv, dot(Qux, x))
+    ea1 = dot(Wa10.T, x) + term[0, :]
+    ea2 = dot(Wa20.T, x) + term[1, :]
+    ea3 = dot(Wa30.T, x) + term[2, :]
+    ea4 = dot(Wa40.T, x) + term[3, :]
     return
     
 def Q_learning_dynamics(x1, x2, S):
