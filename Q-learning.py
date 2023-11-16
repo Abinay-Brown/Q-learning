21390bbafa77f8458ac67e64920ae679099d2e25
Abinay-Brown <abinay.joel@gmail.com>
2023-11-16 04:23:28 -0500

    Complete BabyCT_B

--- Q-learning.py
+++ Q-learning.py
@@ -2,7 +2,7 @@ import numpy as np
 import control as ct
 from params import *
 from dynamics import *
-from numpy import sin, cos, tan, ones, dot, hstack, vstack, zeros
+from numpy import sin, cos, tan, ones, dot, hstack, vstack, zeros, square
 from numpy.linalg import norm, inv
 from scipy.interpolate import CubicSpline
 
@@ -19,67 +19,40 @@ def interp2PWC(y, xi, xf):
     return itp(xdata)
 
 
-# Linearized inertial frame
-def state_space(state, inputs):
-    x, y, z, xd, yd, zd = state;
-    phi, theta, psi, U1 = inputs;
-    
-    A = np.zeros((6, 6))
-    B = np.zeros((6, 4))
-     
-    
-    A[0, 3] = 1;
-    A[1, 4] = 1;
-    A[2, 5] = 1;
-    
-    B[3, 0] = (U1/mass)*((cos(phi)*sin(psi))-(cos(psi)*sin(phi)*sin(theta)))
-    B[4, 0] =-(U1/mass)*((cos(phi)*cos(psi))+(sin(phi)*sin(psi)*sin(theta)))
-    B[5, 0] =-(U1/mass)*((cos(theta)*sin(phi)))
-    
-    B[3, 1] = (U1/mass)*(cos(phi)*cos(psi)*cos(theta))
-    B[4, 1] = (U1/mass)*(cos(phi)*cos(theta)*sin(psi))
-    B[5, 1] =-(U1/mass)*(cos(phi)*sin(theta))
-    
-    B[3, 2] = (U1/mass)*((cos(psi)*sin(phi))-(cos(phi)*sin(psi)*sin(theta)))
-    B[4, 2] = (U1/mass)*((sin(phi)*sin(psi))+(cos(phi)*cos(psi)*sin(theta)))
-    
-    B[3, 3] = (1/mass)*((sin(phi)*sin(psi))+(cos(phi)*cos(psi)*sin(theta)))
-    B[4, 3] =-(1/mass)*((cos(psi)*sin(phi))-(cos(phi)*sin(psi)*sin(theta)))
-    B[5, 3] = (1/mass)*(cos(phi)*cos(theta))
-    
-    return A, B
 
 def babyCT_b(t, state, params):
-    # Extract Drone dynamics Parameters
+
     # Extract Q-learning dynamics parameters
+    # state = [phi,theta, psi, p, q, r, u, v, w, X, Y, Z, Wc0(55), Wa0(6), Wa10(6), Wa20(6), Wa30(6), Wa40(6), P(1)]
     xf, T, Tf, M, R, Pt, percent, amplitude, alpha_a, alpha_c, u1_del, u2_del, u3_del, u4_del, x1_del, x2_del, x3_del, x4_del, x5_del, x6_del, p_del, u_save = params 
     n, m = 6, 4
-    # state = [phi,theta, psi, p, q, r, u, v, w, X, Y, Z, Wc0(55), Wa0(6), Wa10(6), Wa20(6), Wa30(6), Wa40(6), P(1)]
-    # x = [X, Y, Z, U, V, W]
     
+    
+    # Extract Drone dynamics Parameters
+    drone_state = state[0:12]
     phi, theta, psi, p, q, r, u, v, w, X, Y, Z = state[0:12]
-    Wc = state[12:67]
-    Wa10 = state[67: 73]
-    Wa20 = state[73: 79]
-    Wa30 = state[79: 85]
-    Wa40 = state[85: 91]
-    
 
+    # Q-learning Actor and critic weights
+    Wc = state[12:67].reshape(55, 1)
+    Wa10 = state[67: 73].reshape(6, 1)
+    Wa20 = state[73: 79].reshape(6, 1)
+    Wa30 = state[79: 85].reshape(6, 1)
+    Wa40 = state[85: 91].reshape(6, 1)
     P = state[91]
     
-    
+    # Q-learning state vector is global position and velocity
+    # x = [X, Y, Z, U, V, W]
     R_mat = Rot(phi, theta, psi)
     Vels = np.dot(R_mat, np.array([u, v, w]).T)
     U = Vels[0]
     V = Vels[1]
     W = Vels[2]
-    
-    # Q-learning state vector is global position and velocity
     x = np.array([X, Y, Z, U, V, W]).reshape(6, 1)
     
     # Updated Control
-    ud = np.array([dot(Wa10.T, x-xf), dot(Wa20.T, x-xf), dot(Wa30.T, x-xf), dot(Wa40.T, x-xf)])
+    ud = np.array([dot(Wa10.T, x-xf), dot(Wa20.T, x-xf), dot(Wa30.T, x-xf), dot(Wa40.T, x-xf)]).reshape(4, 1)
     
+    # Interpolate delayed control and state
     ud_del = zeros(m, 1)
     x_del  = zeros(n, 1)
     
@@ -100,27 +73,27 @@ def babyCT_b(t, state, params):
     diff = x-xf
     U = np.array([diff[0], diff[1], diff[2], diff[3], diff[4], diff[5], ud[0], ud[1], ud[2],ud[3], ud[4]]).T
     UkU = np.array([U[0]**2, U[0]*U[1], U[0]*U[2], U[0]*U[3], U[0]*U[4], U[0]*U[5], U[0]*U[6], U[0]*U[7], U[0]*U[8], U[0]*U[9],\
-        U[1]**2, U[1]*U[2], U[1]*U[3], U[1]*U[4], U[1]*U[5], U[1]*U[6], U[1]*U[7], U[1]*U[8], U[0]*U[9],\
-            U[2]**2, U[2]*U[3], U[2]*U[4], U[2]*U[5], U[2]*U[6], U[2]*U[7], U[2]*U[8], U[2]*U[9],\
-                U[3]**2, U[3]*U[4], U[3]*U[5], U[3]*U[6], U[3]*U[7], U[3]*U[8], U[4]*U[9],\
-                    U[4]**2, U[4]*U[5], U[4]*U[6], U[4]*U[7], U[4]*U[8], U[4]*U[9],\
-                        U[5]**2, U[5]*U[6], U[5]*U[7], U[5]*U[8], U[6]*U[9],\
-                            U[6]**2, U[6]*U[7], U[6]*U[8], U[6]*U[9],\
-                                U[7]**2, U[7]*U[8], U[7]*U[9],\
-                                    U[8]**2, U[8]*U[9],\
-                                        U[9]**2])	
+    U[1]**2, U[1]*U[2], U[1]*U[3], U[1]*U[4], U[1]*U[5], U[1]*U[6], U[1]*U[7], U[1]*U[8], U[0]*U[9],\
+    U[2]**2, U[2]*U[3], U[2]*U[4], U[2]*U[5], U[2]*U[6], U[2]*U[7], U[2]*U[8], U[2]*U[9],\
+    U[3]**2, U[3]*U[4], U[3]*U[5], U[3]*U[6], U[3]*U[7], U[3]*U[8], U[4]*U[9],\
+    U[4]**2, U[4]*U[5], U[4]*U[6], U[4]*U[7], U[4]*U[8], U[4]*U[9],\
+    U[5]**2, U[5]*U[6], U[5]*U[7], U[5]*U[8], U[6]*U[9],\
+    U[6]**2, U[6]*U[7], U[6]*U[8], U[6]*U[9],\
+    U[7]**2, U[7]*U[8], U[7]*U[9],\
+    U[8]**2, U[8]*U[9],\
+    U[9]**2]).reshape(55, 1)	
     
-    Udel = np.array([x_del[0:6, 0].T, ud_del[0:4, 0].T]).T
+    Udel = hstack((x_del.T, ud_del.T))
     UkUdel = np.array([Udel[0]**2, Udel[0]*Udel[1], Udel[0]*Udel[2], Udel[0]*Udel[3], Udel[0]*Udel[4], Udel[0]*Udel[5], Udel[0]*Udel[6], Udel[0]*Udel[7], Udel[0]*Udel[8], Udel[0]*Udel[9],\
-        Udel[1]**2, Udel[1]*Udel[2], Udel[1]*Udel[3], Udel[1]*Udel[4], Udel[1]*Udel[5], Udel[1]*Udel[6], Udel[1]*Udel[7], Udel[1]*Udel[8], Udel[0]*Udel[9],\
-            Udel[2]**2, Udel[2]*Udel[3], Udel[2]*Udel[4], Udel[2]*Udel[5], Udel[2]*Udel[6], Udel[2]*Udel[7], Udel[2]*Udel[8], Udel[2]*Udel[9],\
-                Udel[3]**2, Udel[3]*Udel[4], Udel[3]*Udel[5], Udel[3]*Udel[6], Udel[3]*Udel[7], Udel[3]*Udel[8], Udel[4]*Udel[9],\
-                    Udel[4]**2, Udel[4]*Udel[5], Udel[4]*Udel[6], Udel[4]*Udel[7], Udel[4]*Udel[8], Udel[4]*Udel[9],\
-                        Udel[5]**2, Udel[5]*Udel[6], Udel[5]*Udel[7], Udel[5]*Udel[8], Udel[6]*Udel[9],\
-                            Udel[6]**2, Udel[6]*Udel[7], Udel[6]*Udel[8], Udel[6]*Udel[9],\
-                                Udel[7]**2, Udel[7]*Udel[8], Udel[7]*Udel[9],\
-                                    Udel[8]**2, Udel[8]*Udel[9],\
-                                        Udel[9]**2])	
+    Udel[1]**2, Udel[1]*Udel[2], Udel[1]*Udel[3], Udel[1]*Udel[4], Udel[1]*Udel[5], Udel[1]*Udel[6], Udel[1]*Udel[7], Udel[1]*Udel[8], Udel[0]*Udel[9],\
+    Udel[2]**2, Udel[2]*Udel[3], Udel[2]*Udel[4], Udel[2]*Udel[5], Udel[2]*Udel[6], Udel[2]*Udel[7], Udel[2]*Udel[8], Udel[2]*Udel[9],\
+    Udel[3]**2, Udel[3]*Udel[4], Udel[3]*Udel[5], Udel[3]*Udel[6], Udel[3]*Udel[7], Udel[3]*Udel[8], Udel[4]*Udel[9],\
+    Udel[4]**2, Udel[4]*Udel[5], Udel[4]*Udel[6], Udel[4]*Udel[7], Udel[4]*Udel[8], Udel[4]*Udel[9],\
+    Udel[5]**2, Udel[5]*Udel[6], Udel[5]*Udel[7], Udel[5]*Udel[8], Udel[6]*Udel[9],\
+    Udel[6]**2, Udel[6]*Udel[7], Udel[6]*Udel[8], Udel[6]*Udel[9],\
+    Udel[7]**2, Udel[7]*Udel[8], Udel[7]*Udel[9],\
+    Udel[8]**2, Udel[8]*Udel[9],\
+    Udel[9]**2]).reshape(55, 1)	
     
     Quu = np.array([[Wc[-10], Wc[-9], Wc[-8], Wc[-7]],
                     [Wc[-9], Wc[-6], Wc[-5], Wc[-4]],
@@ -136,17 +109,66 @@ def babyCT_b(t, state, params):
     dP = 0.5*((dot(x.T, dot(M, x)))+ dot(ud.T, dot(R, ud)))
     sig = UkU - UkUdel
     sig_f = UkU
-    ec = P - p_del_val + dot(Wc.T, sig)
-    ecfinal = 0.5*dot(x.T, dot(Pt, x)) - dot(Wc.T, sig_f)
     
-    term = dot(Quu_inv, dot(Qux, x))
-    ea1 = dot(Wa10.T, x) + term[0, :]
-    ea2 = dot(Wa20.T, x) + term[1, :]
-    ea3 = dot(Wa30.T, x) + term[2, :]
-    ea4 = dot(Wa40.T, x) + term[3, :]
-    return
+    # Critic Errors
+    ec1 = P - p_del_val + dot(Wc.T, sig)
+    ec2 = 0.5*dot(x.T, dot(Pt, x)) - dot(Wc.T, sig_f)
+
+    # Actor Errors
+    Wa = np.hstack((Wa10, Wa20, Wa30, Wa40))
+    ea = dot(Wa.T, x) + dot(Quu_inv, dot(Qux, x))
+    #term = dot(Quu_inv, dot(Qux, x))
+    #ea1 = dot(Wa10.T, x) + term[0, :]
+    #ea2 = dot(Wa20.T, x) + term[1, :]
+    #ea3 = dot(Wa30.T, x) + term[2, :]
+    #ea4 = dot(Wa40.T, x) + term[3, :]
+
+
+    # Critic Dynamics
+    # Check dimensions of output
+    dWc = -alpha_c * ((ec1*sig/square(1+dot(sig.T, sig)))+(ec2*sig_f/square(1+dot(sig_f.T, sig_f))))
     
+    # Actor Dynamics
+    
+    dWa = -alpha_a * dot(x, ea.T)
+    dWa10 = dWa[:, 0]
+    dWa20 = dWa[:, 1]
+    dWa30 = dWa[:, 2]
+    dWa40 = dWa[:, 3]
+
+    unew = zeros(m, 1)
+    # Persistence Excitation
+
+    if t <= (percent/100)*Tf:
+        unew[0, 0] = ud[0] + (amplitude*exp(-0.009*t)*2*(sin(t)**2*cos(t)+sin(2*t)**2*cos(0.1*t)+sin(-1.2*t)**2*cos(0.5*t)+sin(t)**5+sin(1.12*t)**2+cos(2.4*t)*sin(2.4*t)**3))
+        unew[1, 0] = ud[1] + (amplitude*exp(-0.009*t)*2*(sin(t)**2*cos(t)+sin(2*t)**2*cos(0.1*t)+sin(-1.2*t)**2*cos(0.5*t)+sin(t)**5+sin(1.12*t)**2+cos(2.4*t)*sin(2.4*t)**3))
+        unew[2, 0] = ud[2] + (amplitude*exp(-0.009*t)*2*(sin(t)**2*cos(t)+sin(2*t)**2*cos(0.1*t)+sin(-1.2*t)**2*cos(0.5*t)+sin(t)**5+sin(1.12*t)**2+cos(2.4*t)*sin(2.4*t)**3))
+        unew[3, 0] = ud[4] + (amplitude*exp(-0.009*t)*2*(sin(t)**2*cos(t)+sin(2*t)**2*cos(0.1*t)+sin(-1.2*t)**2*cos(0.5*t)+sin(t)**5+sin(1.12*t)**2+cos(2.4*t)*sin(2.4*t)**3))
+    else:
+        unew = ud
+
+    # Convert [phi, theta, psi, altitude].T desired to u1, u2, u3, u4 moment and thrust control inputs using PID
+    PID_update(state, unew) 
+
+    # Do usave in here
+    
+    drone_params = [mass, g, Ixx, Iyy, Izz, l, J, Ct, Ctheta, u1, u2, u3, u4]
+    drone_state_dot = dynamics(t, drone_state, drone_params)
+    phi_dot, theta_dot, psi_dot, p_dot, q_dot, r_dot, u_dot, v_dot, w_dot, U, V, W = drone_state_dot[0:12]
+    
+    dx = np.array([phi_dot, theta_dot, psi_dot, p_dot, q_dot, r_dot, u_dot, v_dot, w_dot, U, V, W]).reshape(1, 12)
+
+    # dWc -> 1x55
+    # dWa10 -> 1x6 
+    dotx = hstack((dx, dWc, dWa10.T, dWa20.T, dWa30.T, dWa40.T, dP))
+    return dotx
+    
+
 def Q_learning_dynamics(x1, x2, S):
+    
+    # State Vector =  [X, Y, Z, VX, VY, VZ]
+    # Control Input = [phi, theta, psi, altitude]
+
     n = 6
     m = 4
     
@@ -181,13 +203,8 @@ def Q_learning_dynamics(x1, x2, S):
     # Appending the vectors after each row
     t_save = hstack(([0],))
     u_save = vstack((u0.T, ))
-    x_save = hstack((xi.T, Wc0.T))
-    x_save = hstack((x_save, Wa10.T))
-    x_save = hstack((x_save, Wa20.T))
-    x_save = hstack((x_save, Wa30.T))
-    x_save = hstack((x_save, Wa40.T))
-    x_save = hstack((x_save, np.array([[0]])))
-    
+    x_save = hstack((xi.T, Wc0.T, Wa10.T, Wa20.T, Wa30.T, Wa40.T, np.array([[0]])))
+
     # Interpolate functions
     u1_del = interp2PWC(u_save[:, 0], -1, 1)
     u2_del = interp2PWC(u_save[:, 1], -1, 1)
